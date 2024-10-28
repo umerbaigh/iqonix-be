a6144c7a9477ad700537d95d6f50030502ed4b03
umerbaigh <umerbaig.spada@gmail.com>
2024-10-28 23:01:03 +0500

    Initial commit

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,6 +2,11 @@ const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("MY_HEROKU_URL"));
+  console.log("host=====>", host);
+  console.log("port=====>", port);
+  console.log("database=====>", database);
+  console.log("user=====>", user);
+  console.log("password=====>", password);
 
   return {
     connection: {
